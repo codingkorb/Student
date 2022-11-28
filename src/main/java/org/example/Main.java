0243545a9d5bbd804a68cdeb24391943844a7342
harrykorb <codingkorb@gmail.com>
2022-11-28 17:12:14 +0100

    1. Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import de.neuefische.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student George = new Student("George", "Foreman", 34);
+    }
+    
+
+
+}
---
+++ src/test/java/de/neuefische/StudentTest.java
@@ -0,0 +1,38 @@
+package de.neuefische;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+}
